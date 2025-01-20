416f69e7395967d307a4fee6105f82b46cf5ef87
Feihuan Peng <ericrealtor1997@gmail.com>
2025-01-19 23:14:56 -0800

    player movements and dash ability

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,16 +2,27 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    private Rigidbody2D rb;
-    private Animator anim;
-
     [SerializeField] float moveSpeed = 5f;
     [SerializeField] float jumpForce = 7f;
 
-    float xInput;
+    [Header("Dash Info")]
+    [SerializeField] float dashDuration;
+    [SerializeField] float dashSpeed;
+    [SerializeField] float dashCoolDown;
 
+    [Header("Collision Info")]
+    [SerializeField] float groundCheckDistance;
+    [SerializeField] LayerMask whatIsGround;
+
+    private Rigidbody2D rb;
+    private Animator anim;
+
+    float xInput;
+    bool isGrounded;
     int facingDir = 1;
     bool facingRight = true;
+    [SerializeField]float dashTime;
+    [SerializeField]float dashCoolDownTimer;
 
     void Start()
     {
@@ -23,8 +34,19 @@ public class Player : MonoBehaviour
     {
         Movement();
         Checkinput();
+        CollisionChecks();
+
+        
+        if(dashTime > 0) dashTime -= Time.deltaTime;
+        if(dashCoolDownTimer > 0) dashCoolDownTimer -= Time.deltaTime;
+
         FlipController();
         AnimationControllers();
+    }
+
+    private void CollisionChecks()
+    {
+        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, whatIsGround);
     }
 
     private void Checkinput()
@@ -34,23 +56,46 @@ public class Player : MonoBehaviour
         {
             Jump();
         }
+
+        if(Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            DashAbility();
+        }
+    }
+
+    private void DashAbility()
+    {
+        if(dashCoolDownTimer <= 0)
+        {
+            dashCoolDownTimer = dashCoolDown;
+            dashTime = dashDuration;
+        }
     }
 
     private void Movement()
     {
-        rb.linearVelocity = new Vector2(xInput * moveSpeed, rb.linearVelocity.y);
+        if(dashTime > 0)
+        {
+            rb.linearVelocity = new Vector2(xInput * dashSpeed, 0);
+        }
+        else{
+            rb.linearVelocity = new Vector2(xInput * moveSpeed, rb.linearVelocity.y);
+        }
     }
 
     private void Jump()
     {
-        rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        if(isGrounded)
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
     }
 
     private void AnimationControllers()
     {
         bool isMoving = rb.linearVelocityX != 0;
-
         anim.SetBool("isMoving", isMoving);
+        anim.SetBool("isGrounded", isGrounded);
+        anim.SetFloat("yVelocity", rb.linearVelocityY);
+        anim.SetBool("isDashing", dashTime > 0);
     }
 
     private void FlipPlayer()
@@ -62,14 +107,17 @@ public class Player : MonoBehaviour
 
     private void FlipController()
     {
-        if(rb.linearVelocityX > 0 && !facingRight)
+        if (rb.linearVelocityX > 0 && !facingRight)
             FlipPlayer();
-        else if(rb.linearVelocityX < 0 && facingRight)
+        else if (rb.linearVelocityX < 0 && facingRight)
             FlipPlayer();
     }
 
-
-
+// // debug function, draw a line in scene
+//     private void OnDrawGizmos()
+//     {
+//         Gizmos.DrawLine(transform.position, new Vector3(transform.position.x, transform.position.y - groundCheckDistance));
+//     }
 
     private static void TwoWayGetAxisInput()
     {
